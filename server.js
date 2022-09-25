af00c4e48f4e20d7ee79df86efbf267ba64b5f8d
DavidaaWoW <davidik.gegiya@gmail.com>
2022-09-25 12:23:22 +0300

    update server.js

--- server.js
+++ server.js
@@ -78,6 +78,6 @@ app.delete("/contact/:id", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Сервер ожидает подключения...");
 });
